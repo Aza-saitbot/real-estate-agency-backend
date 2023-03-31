4e7472a4b503ba83705087e69a4ce9bc02364cb1
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2023-03-31 19:43:30 +0300

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import {Controller, Get} from "@nestjs/common";
+import {AppService} from "./app.service";
+
+@Controller('api')
+export class AppController {
+
+    constructor(private appService:AppService) {
+    }
+
+
+    @Get('users')
+    getUser() {
+        return this.appService.getUser()
+    }
+
+}
