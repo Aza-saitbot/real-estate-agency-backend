0ee2961e2ebe8765897f579128c208f388a1efb6
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2023-04-04 20:53:56 +0300

    feat/added methods login and registration v1

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import {forwardRef, Module} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import {AuthController} from './auth.controller';
 import {UsersModule} from "../users/users.module";
 import {JwtModule} from "@nestjs/jwt";
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
-  imports:[
-      UsersModule,
-      JwtModule.register({
-          secret:process.env.PRIVATE_KEY || 'Secret',
-          signOptions:{
-              expiresIn:'24h'
-          }
-      })
-  ],
+    controllers: [AuthController],
+    providers: [AuthService],
+    imports: [
+        forwardRef(() => UsersModule),
+        JwtModule.register({
+            secret: process.env.PRIVATE_KEY || 'Secret',
+            signOptions: {
+                expiresIn: '24h'
+            }
+        })
+    ],
     exports: [
         AuthService,
         JwtModule
     ]
 })
-export class AuthModule {}
+export class AuthModule {
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
 import {UsersService} from './users.service';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./users.model";
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
 
 @ApiTags('Пользователи')
 @Controller('users')
@@ -19,8 +20,11 @@ export class UsersController {
 
     @ApiOperation({summary: 'Получить всех пользователей'})
     @ApiResponse({status: 200, type: [User]})
+    @UseGuards(JwtAuthGuard)
     @Get()
     getAll() {
         return this.usersService.getAllUser()
     }
 }
+
+
